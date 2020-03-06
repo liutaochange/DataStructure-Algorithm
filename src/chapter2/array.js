f128836d8871106c189732050160d37980f7d499
liutaochange <liutaochange@163.com>
2020-03-06 16:25:02 +0800

    feat: chapter2记录

--- src/chapter2/array.js
+++ src/chapter2/array.js
@@ -88,8 +88,71 @@ print(iterator1.next().value) // [ 0, 'a' ]
 print(iterator1.next().value) // [ 1, 'b' ]
 // every() 方法测试一个数组内的所有元素是否都能通过某个指定函数的测试。它返回一个布尔值。
 // every() 方法 若收到一个空数组，此方法在一切情况下都会返回 true。
-// every 不会改变原数组。cls
+// every() 不会改变原数组。
 const isBelowThreshold = currentValue => currentValue < 40
 const every1 = [1, 30, 39, 29, 10, 13]
 print(every1.every(isBelowThreshold))
+// some() 方法测试数组中是不是至少有1个元素通过了被提供的函数测试。它返回的是一个Boolean类型的值。
+// some() 如果用一个空数组进行测试，在任何情况下它返回的都是false。
+// some() 被调用时不会改变数组。
+const some = [1, 2, 3, 4, 5]
+const even = element => element % 2 === 0
+print(some.some(even)) // true
+// reduce() 方法对数组中的每个元素执行一个由您提供的reducer函数(升序执行)，将其结果汇总为单个返回值。
+// reducer 函数接收4个参数:
+// Accumulator (acc) (累计器)
+// Current Value (cur) (当前值)
+// Current Index (idx) (当前索引)
+// Source Array (src) (源数组)
+const reduce = [1, 2, 3, 4]
+const reducer = (accumulator, currentValue) => accumulator + currentValue
+print(reduce.reduce(reducer))
+// reduceRight() 方法接受一个函数作为累加器（accumulator）和数组的每个值（从右到左）将其减少为单个值。
+const reduceRight = [
+  [0, 1],
+  [2, 3],
+  [4, 5]
+].reduceRight((accumulator, currentValue) => accumulator.concat(currentValue))
+print(reduceRight)
+// find() 方法返回数组中满足提供的测试函数的第一个元素的值。否则返回 undefined。
+const find = [5, 12, 8, 130, 44]
+const found = find.find(element => element > 10)
+print(found)
+// findIndex()方法返回数组中满足提供的测试函数的第一个元素的索引。否则返回-1。
+const findIndex = [5, 12, 8, 130, 44]
+const isLargeNumber = element => element > 13
+print(findIndex.findIndex(isLargeNumber))
+// includes() 方法用来判断一个数组是否包含一个指定的值，根据情况，如果包含则返回 true，否则返回false。
+const in1 = [1, 2, 3]
+print(in1.includes(2)) // true
+const pets = ['cat', 'dog', 'bat']
+print(pets.includes('cat')) // true
+print(pets.includes('at')) // false
+// filter() 方法创建一个新数组, 其包含通过所提供函数实现的测试的所有元素。如果没有任何数组元素通过测试，则返回空数组。
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']
+const result = words.filter(word => word.length > 6)
+print(result) // ["exuberant", "destruction", "present"]
 // fill() 方法用一个固定值填充一个数组中从起始索引到终止索引内的全部元素。不包括终止索引。
+// fill 方法接受三个参数 value, start 以及 end. start 和 end 参数是可选的, 其默认值分别为 0 和 this 对象的 length 属性值。
+// 如果 start 是个负数, 则开始索引会被自动计算成为 length+start, 其中 length 是 this 对象的 length 属性值。如果 end 是个负数, 则结束索引会被自动计算成为 length+end。
+const fill1 = [1, 2, 3, 4]
+print(fill1.fill(0, 2, 4)) // [ 1, 2, 0, 0 ]
+// map() 方法创建一个新数组，其结果是该数组中的每个元素都调用一个提供的函数后返回的结果。
+const map = [1, 4, 9, 16]
+const map1 = map.map(x => x * 2)
+print(map1)
+// flat() 方法会按照一个可指定的深度递归遍历数组，并将所有元素与遍历到的子数组中的元素合并为一个新数组返回。
+// Polyfill
+function flatten(array) {
+  var flattend = []
+  ;(function flat(array) {
+    array.forEach(function(el) {
+      if (Array.isArray(el)) flat(el)
+      else flattend.push(el)
+    })
+  })(array)
+  return flattend
+}
+const flat = [1, 2, [3, 4]]
+// flat.flat()
+print(flatten(flat))
